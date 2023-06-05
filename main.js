d30b10f8526e4e36f26283427d33380a43c3e503
DaviLeoMeda <davileo.meda@gmail.com>
2023-06-05 13:43:25 +0200

    Starting project

---
+++ main.js
@@ -0,0 +1,16 @@
+const { createApp } = updated()
+
+createApp({
+    data() {
+        return {
+            apiUrl: '',
+            data: ''
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}).mount('#app')
